de3b1a22b7fccb9042f85b2953eb1e710d8cb882
Noya Arav <noya.arav@post.runi.ac.il>
2024-06-11 12:32:23 +0300

    analyses sentiments- very basic

--- Project.py
+++ Project.py
@@ -2,14 +2,20 @@ import assemblyai as aai
 
 aai.settings.api_key = "d7e18dd00ed3408dbdd1919c223752f1"
 
-audio_file = "/Users/noyaarav/Desktop/Final-Project-From-Idea-To-Reality/audio_files/test1.mp3"
+audio_file = "/Users/noyaarav/Desktop/Final-Project-From-Idea-To-Reality/audio_files/test.mp3"
 
 config = aai.TranscriptionConfig(
   speaker_labels=True,
-  speakers_expected=2
+  speakers_expected=2,
+  sentiment_analysis=True
 )
 
 transcript = aai.Transcriber().transcribe(audio_file, config)
 
-for utterance in transcript.utterances:
-  print(f"Speaker {utterance.speaker}: {utterance.text}")
+# for utterance in transcript.utterances:
+#   print(f"Speaker {utterance.speaker}: {utterance.text}")
+  
+for sentiment_result in transcript.sentiment_analysis:
+    print(f"Speaker {sentiment_result.speaker}: {sentiment_result.text}")
+    print(sentiment_result.sentiment)  # POSITIVE, NEUTRAL, or NEGATIVE
+    print(f"Confidence in sentiment: {sentiment_result.confidence}")
